cd15dc92240fc807524e50c3c22c7725ee1b5c6d
Sanchy <vsanchez.gassull@gmail.com>
2018-05-14 19:36:07 +0200

    tornant a començar

--- game/AINull.cc
+++ game/AINull.cc
@@ -1,38 +1,81 @@
 #include "Player.hh"
-
+#include <utility>
+#include <queue>
+using namespace std;
 
 /**
- * Write the name of your player and save this file
- * with the same name and .cc extension.
- */
-#define PLAYER_NAME Null
+* Write the name of your player and save this file
+* with the same name and .cc extension.
+*/
+#define PLAYER_NAME Maincra
 
 
 struct PLAYER_NAME : public Player {
 
-  /**
-   * Factory: returns a new instance of this class.
-   * Do not modify this function.
-   */
-  static Player* factory () {
-    return new PLAYER_NAME;
-  }
+    /**
+    * Factory: returns a new instance of this class.
+    * Do not modify this function.
+    */
+    static Player* factory () {
+        return new PLAYER_NAME;
+    }
 
-  /**
-   * Types and attributes for your player can be defined here.
-   */
+    using VE = vector <int>;
+    using VVE = vector <VE>;
+    using P = pair <int,int>;
 
-  /**
-   * Play method, invoked once per each round.
-   */
-  virtual void play () {
 
-  }
+    //FOREST GRASS WATER MOUNTAIN
+
+    /* NAPALM
+
+    * * * * *
+    * * * * *
+    * * H * *
+    * * * * *
+    * * * * *
+
+    */
+
+    VVE visitats;
+    VE v_soldiers;
+    VE v_helicopters;
+    vector <Post> v_posts;
+
+    /*bool napalm_QuestionMark(P pos) {
+        int num_enemics = 0;
+        int num_meus = 0;
+        bool yes_OMG = false;
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j) {
+                if ([pos.x - 2 + i][pos.x - 2 + j])
+                    ++ num_enemics;
+                if ([pos.x - 2 + i][pos.x - 2 + j])
+                    ++num_meus;
+
+            }
+        return (num_enemics >  or (num_meus < 3 and num_enemics > num_meus) or (num_enemics > 2*num_meus-1))
+    }*/
+
+    /**
+    * Play method, invoked once per each round.
+    */
+    virtual void play () {
+
+        if (status() > 0.95) return;
+        if (round() == 0){
+            v_posts = posts();
+            int post_size = v_posts.size();
+            for (int i = 0; i < post_size; ++i) {
+
+            }
+        }
+    }
 
 };
 
 
 /**
- * Do not modify the following line.
- */
+* Do not modify the following line.
+*/
 RegisterPlayer(PLAYER_NAME);
--- game/MyPlayer.cc
+++ game/MyPlayer.cc
@@ -1,113 +1,33 @@
 #include "Player.hh"
-#include <cmath>
-#include <stack>
-
 
 
 /**
-* Write the name of your player and save this file
-* with the same name and .cc extension.
-*/
-#define PLAYER_NAME Maincra
+ * Write the name of your player and save this file
+ * with the same name and .cc extension.
+ */
+#define PLAYER_NAME Null
 
 
 struct PLAYER_NAME : public Player {
 
-/**
-* Factory: returns a new instance of this class.
-* Do not modify this function.
-*/
+  /**
+   * Factory: returns a new instance of this class.
+   * Do not modify this function.
+   */
+  static Player* factory () {
+    return new PLAYER_NAME;
+  }
 
-    static Player* factory () {
-        return new PLAYER_NAME;
-    }
+  /**
+   * Types and attributes for your player can be defined here.
+   */
 
-    using VE = vector <int>;
-    using VVE = vector <VE>;
-    using VP = vector <Post>;
-    using P = pair <int,int>;
-    using Q = queue<P>;
-    const int my_id = me();
-    int num_soldiers = 20;
-    int num_parachuters = 0;
+  /**
+   * Play method, invoked once per each round.
+   */
+  virtual void play () {
 
-    VVE visitats;
-    vector <Q> cues;
-
-/*
-    VE v_soldiers (20);
-    VE v_helicopters (2);
-    VE v_parachuters (0);
-    VP post_no_capturats (16);
-    VP v_posts (16);
-    S s;
-*/
-
-    bool no_possibru(P p) {
-        return (p.first < 0 and p.first >= MAX and p.second >= MAX and p.second < 0 or fire_time(p.first,p.second) > 1 or what(p.first,p.second) == 4);
-    }
-
-    bool cubito(int x, int y, queue<P> &Q, int d) {
-    	if (no_possibru({x,y})) return false;
-        visitats[x][y] = true;
-    	return true;
-    }
-
-    Q BFS(int i, int j) {
-    	Q q;
-    	q.push(P(i,j));
-    	while (not q.empty()) {
-    		P v = q.front();
-    		q.pop();
-    		if (cubito(x,y+1,q,d) or cubito(x,y-1,q,d) or cubito(x+1,y,q,d) or cubito(x-1,y,q,d)) {
-    			return q;
-    		}
-
-    	}
-    	cout << "no treasure can be reached" << endl;
-    }
-
-/*void dijkstra(constWGraph& G,int s, vector<double>& d, vector<int>& p) {
-    int n = G.size();
-    d = vector<double>(n, infinit);
-    d[s] = 0;
-    p = vector<int>(n, -1);
-    vector<boolean> S(n,false);
-    priority_queue<WArc, vector<WArc>, greater<WArc> > Q;
-    Q.push(WArc(0, s));
-    while(not Q.empty()) {
-        int
-        u = Q.top().second;  Q.pop();
-        if(not S[u]) {
-            S[u] = true;
-            for(WArc a : G[u]) {
-                int v = a.second;
-                double c = a.first;
-                if (d[v] > d[u] + c) {
-                    d[v] = d[u] + c;
-                    p[v] = u;
-                    Q.push(WArc(d[v], v));
-                }
-            }
-        }
-    }
-}*/
-
-    virtual void play () {
-        vsoldiers = soldiers(my_id);
-        int n = vsoldiers.size();
-        vhelicopters = helicopters(my_id);
-        cues = vector <Q> (vsoldiers.size());
-        vposts = posts();
-        visitats = VVE(MAX,VE(MAX,false));
-        if (round() == 0)
-            vposts = posts();
-        else {
-            for (int i = 0; i < n; ++i) {
-                cues[i] = BFS();
-            }
-        }
-    }
+  }
 
 };
 
